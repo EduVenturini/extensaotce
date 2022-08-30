13d130291a49359b8fcd8010a5af98b2354e7ecf
EduVenturini <111748138+EduVenturini@users.noreply.github.com>
2022-08-29 22:39:59 -0300

    Update aritmetica.py

--- Projeto - calculadora/aritmetica.py
+++ Projeto - calculadora/aritmetica.py
@@ -6,37 +6,27 @@ e um módulo - chamado aritmetico.py - além de módulos que forneçam
 funções de entrada e saída de dados.
 Autor: Eduardo S Venturini
 Data: 29/08/2022
-Versão: 0.0.6
+Versão: 0.0.8
 """
 
 #Processamento de Dados
-#O cálculo é feito aqui com o x, y e OP
 
 def adicao(x: float, y: float) -> float:
-    """
-    Docstring de documentação da função.  Esta função soma dois números.
-    """
+    #soma dois números.
     return x + y
 
 
 def subtracao(x: float, y: float) -> float:
-   """
-   Esta função subtrai um número y de um número x.
-   """
+   #subtrai um número x de um número y.
    return x - y
 
 def multiplicacao(x: float, y: float) -> float:
-    """
-    Esta função multiplica dois números.
-    """
+    #multiplica os dois números.
     return x*y
 
 
 def divisao(x: float, y: float) -> float:
-    """
-    Esta função pega dois números tais que o segundo é diferente de zero
-    e calcula a sua divisão.
-    """
+    #divide x por y sendo y diferente de zero
     if y == 0:
         return "Não se pode dividir por zero."
     return x/y
